0aaca9998294dd3af4c8fcbc8d9061dd3f4c67be
junho <>
2025-12-15 12:22:37 +0900

    feat: 장바구니 화면 구현

--- components/cart/cartSummaryItem.tsx
+++ components/cart/cartSummaryItem.tsx
@@ -1,11 +1,63 @@
+import { TCartSummaryItem } from '@/types/cartTypes'
 import React from 'react'
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 
-const CartSummaryItem = () => {
+type Props = {
+  item: TCartSummaryItem;
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+};
+
+const CartSummaryItem = ({ item, checked, onCheckedChange }: Props) => {
   return (
-    <div>
-      
+    <div
+      className="
+        relative
+        flex items-center gap-4
+        border border-[#2E2B2F]/20
+        rounded-lg p-4
+        w-full sm:max-w-[90vw] md:max-w-[640px] lg:max-w-[720px]
+      "
+    >
+      <Button
+        type="button"
+        variant="link"
+        className="
+          absolute top-1 right-4
+          text-[#2E2B2F]
+          text-sm
+          px-0
+        "
+      >
+        삭제
+      </Button>
+      <Checkbox
+        checked={checked}
+        onCheckedChange={(value) => onCheckedChange(!!value)}
+      />
+      <img
+        src={item.url}
+        alt={item.title}
+        className="
+          w-24 h-24
+          rounded-md
+          object-cover
+          bg-gray-100
+        "
+      />
+
+      <div className="flex flex-col gap-1 flex-1">
+        <p className="text-base font-medium line-clamp-2">
+          {item.title}
+        </p>
+
+        <p className="text-lg font-semibold">
+          {item.price.toLocaleString()}원
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CartSummaryItem
--- components/userMenu.tsx
+++ components/userMenu.tsx
@@ -1,13 +1,25 @@
+"use client"
 import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import UserDeposit from './userDeposit'
+import { useRouter } from 'next/navigation'
 
 const UserMenu = () => {
+
+  const router = useRouter(); 
+  const moveToCart = () => {
+    router.push('/cart/checkout')
+  }
   return (
     <div className="flex justify-center items-center gap-4">
       <UserDeposit/>
-      <FontAwesomeIcon icon={faCartShopping} size="lg" className='hover:cursor-pointer'/>
+      <FontAwesomeIcon 
+        icon={faCartShopping} 
+        size="lg" 
+        className='hover:cursor-pointer'
+        onClick={() => moveToCart()}
+      />
       <FontAwesomeIcon icon={faUser} size="lg" className='hover:cursor-pointer'/>
     </div>
   )
